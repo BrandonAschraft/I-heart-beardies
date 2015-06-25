3c5974f2331415ba1c75e84a660a927658abdd09
brandonaschraft <b.ashcraft502@gmail.com>
2015-06-25 07:46:59 +0000

    slideshow- ITS ALIVEgit rm site-images/french-resize_480-230.jpg

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
-// $(document).ready(function() {
-//   var $pics = "#slideshow li";
-//   // $($pics).hide();
-//   setInterval(function() {
-//     $($pics).each(function() {
-//       $(this).fadeIn(2000).delay(3000).fadeOut(2000);
-//       $(".wait").delay(5000);
-//     });
-//   }, 1000);
-// });
+$(document).ready(function() {
+  var $pics = "#slideshow div";
+   $($pics).hide();
+  setInterval(function() {
+    $($pics).each(function() {
+      $(this).fadeIn(2000).delay(3000).fadeOut(2000);
+      $(".wait").delay(5000);
+    });
+  }, 1000);
+});
+
+console.log("test");
 
