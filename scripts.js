927a9410bf1be6dd585c3649282eac936ed189c3
brandonaschraft <b.ashcraft502@gmail.com>
2015-06-25 03:56:46 +0000

    gotta check out the other branch..

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
-
-
+// $(document).ready(function() {
+//   var $pics = "#slideshow li";
+//   // $($pics).hide();
+//   setInterval(function() {
+//     $($pics).each(function() {
+//       $(this).fadeIn(2000).delay(3000).fadeOut(2000);
+//       $(".wait").delay(5000);
+//     });
+//   }, 1000);
+// });
 
