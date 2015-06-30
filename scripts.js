30ab07c2eeb578a9d3c65a09e93e28bf122d3bc5
brandonaschraft <b.ashcraft502@gmail.com>
2015-06-30 07:47:21 +0000

    PetCare page

--- scripts.js
+++ scripts.js
@@ -11,3 +11,29 @@ $(document).ready(function() {
 
 console.log("test");
 
+//********* PETCARE html BUTTONS*******
+
+// 1 Hide text elements
+var hidden= $('.hide');
+
+ hidden.hide();
+ 
+
+// 2 show button spans for 'enclosures', 'lighting/Heat', 'Water/Bathing'
+            // 2a DO THIS FOR ALL TOPICS IN EACH SECTION
+
+$("#enclosures").append('<button class="textReveal">ENCLOSURES</button>');
+$("#lighting").append('<button class="textReveal">LIGHTING AND HEAT</button>');
+$("#water").append('<button class="textReveal">WATER</button>');
+$("#overview").append('<button class="textReveal">OVERVIEW</button>');
+$("#veggies").append('<button class="textReveal">FRUITS AND VEGETABLES</button>');
+$("#bugs").append('<button class="textReveal">FEEDER ANIMALS</button>')
+$(".textReveal").click(function(){
+    $(this).parent().next().slideToggle();
+  });
+  
+
+
+// 3 on toggle 
+//     show/hide text
+
