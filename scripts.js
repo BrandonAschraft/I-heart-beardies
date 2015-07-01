53854464d47bb91f89c8a6d3f5dbd3f5b4594b82
brandonaschraft <b.ashcraft502@gmail.com>
2015-07-01 22:29:18 +0000

    petcare update, scetchy slideshow

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   var $pics = "#slideshow div";
    $($pics).hide();
-  setInterval(function() {
-    $($pics).each(function() {
+   setInterval(function() {
+    $($pics).each(function(index) {
       $(this).fadeIn(2000).delay(3000).fadeOut(2000);
-      $(".wait").delay(5000);
+         $(".wait").delay(5000); 
+      
     });
-  }, 1000);
+   },0);
 });
 
-console.log("test");
+ 
+
+
 
 //********* PETCARE html BUTTONS*******
 
@@ -27,13 +30,16 @@ $("#lighting").append('<button class="textReveal">LIGHTING AND HEAT</button>');
 $("#water").append('<button class="textReveal">WATER</button>');
 $("#overview").append('<button class="textReveal">OVERVIEW</button>');
 $("#veggies").append('<button class="textReveal">FRUITS AND VEGETABLES</button>');
-$("#bugs").append('<button class="textReveal">FEEDER ANIMALS</button>')
+$("#bugs").append('<button class="textReveal">FEEDER ANIMALS</button>');
+$("#nutrition").append('<button class="textReveal">NUTRITION INFORMATION</button>');
+$("#notFood").append('<button class="textReveal">FOODS TO AVOID</button>');
+$("#cohabitation").append('<button class="textReveal">COHABITATION</button>');
+// 3 on toggle, show/hide text
 $(".textReveal").click(function(){
     $(this).parent().next().slideToggle();
   });
   
 
 
-// 3 on toggle 
-//     show/hide text
+
 
